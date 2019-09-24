57864e4ed53543d557c07135f6b85ca065fffddf
Kiza-coder <kiza.motoko@gmail.com>
2019-09-24 12:34:38 +0200

    Update js

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-let myStock = sessionStorage;
-myStock.setItem(1,1);
-console.log(myStock.getItem(1));
+// Local  storage array with global variabl (key,value)
+let myStock = localStorage;
+myStock.setItem("score",0);
 
 
 
-
-document.getElementById("run").addEventListener("click",() =>{
-
-	let score = parseInt(document.getElementById("target").innerHTML)
-		
+//function who increment  the score of one
+function incrementOne(){
+		let score = parseInt(myStock.getItem("score"))
 		score ++;
 		console.log(score)	
 		target.innerHTML = score;
+		myStock.setItem("score",score)
+}
+
+
+//function who multiplcate the score
+function multiplication(num)
+	{
+		let score = myStock.getItem("score")
+		score = score * num
+		target.innerHTML = score;
+		myStock.setItem("score",score)
+	}
+
+
+//function who automatically increment the score of "num" every 1000 ms
+function autoClick(num)
+	{
+		console.log(myStock.getItem("score"))
+		setInterval(function(){
+			let score = parseInt(myStock.getItem("score"));
+			score =parseInt(score+num);
+			target.innerHTML = score;
+			myStock.setItem("score",score);
+		},1000);
+	}
+
+//function who decrement the score after a purchase
+function buy()
+	{
+
+	}	
+	
 
 
 
-
-
-
+document.getElementById("run-2").addEventListener("click",() =>{
+	autoClick(1);
 });
+
+
+document.getElementById("run").addEventListener("click",() =>{
+	autoClick();
+	});
 
 
 
